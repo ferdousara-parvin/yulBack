0b818172695cc7043cf57221132b22ed4c6bc2ea
Hambrsoom <hampico1234@gmail.com>
2021-01-23 21:21:45 -0500

    US-1: fixed merging conflicts

--- src/main/java/ca/onepoint/yul/controller/AvatarController.java
+++ src/main/java/ca/onepoint/yul/controller/AvatarController.java
@@ -1,17 +1,23 @@
 package ca.onepoint.yul.controller;
 
 import ca.onepoint.yul.dto.AvatarDto;
+import ca.onepoint.yul.dto.MapDto;
 import ca.onepoint.yul.service.IAvatarService;
+import ca.onepoint.yul.service.IMapService;
+import ca.onepoint.yul.utility.Movement;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,6 +26,9 @@ public class AvatarController {
 
     @Resource
     private IAvatarService iAvatarService;
+
+    @Resource
+    private IMapService iMapService;
 
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
@@ -49,14 +58,20 @@ public class AvatarController {
     @CrossOrigin
     @GetMapping("/type/{type}")
     public List<AvatarDto> findAvatarsByType(@PathVariable Integer type) {
-        return iAvatarService.getAvatarsByType(type);
+         return iAvatarService.getAvatarsByType(type);
     }
 
     @CrossOrigin
     @PostMapping("/move-avatars")
-    public void moveAvatars(@RequestBody List<AvatarDto> listAvatar) {
-        //TODO: Update the avatar x, y
+    public void moveAvatars(@RequestBody List<AvatarDto> listAvatar) throws JSONException, JsonProcessingException {
+        long id = 1;
+        MapDto map = iMapService.getMapById(id);
+
+        for (int i = 0; i < listAvatar.size(); i++) {
+            listAvatar.set(i, Movement.move(listAvatar.get(i), map));
+        }
         messagingTemplate.convertAndSend("/topic/progress", listAvatar);
     }
+
 
 }
