beacc5efaad84afefbcda0bd7aafefa757c28263
Viveka <viveka98@hotmail.ca>
2021-02-01 17:00:07 -0500

    Extracted protestors method into multiple small methods

--- src/main/java/ca/onepoint/yul/classes/ManifestationManagement.java
+++ src/main/java/ca/onepoint/yul/classes/ManifestationManagement.java
@@ -12,54 +12,97 @@ import java.util.List;
 import java.util.Random;
 
 public class ManifestationManagement {
+    //Constants
+    public static final int THRESHOLD = 15;
+    public static final int MAP_SIZE = 30;
+    public static final String PROTESTERS_IMAGE = "../assets/images/protester.png";
+
+    // Global variables
     public static ArrayList<AvatarDto> allProtesters = new ArrayList<>();
+    private static boolean protestorsBlockVerticalStreet;
+    private static int randomStreetIndex;
+    private static SquareDto[][] square;
 
     public static List<AvatarDto> getProtestors(IMapService mapService) {
+        MapDto map = getFirstMap(mapService);
+
+        if (map == null) {
+            return new ArrayList<>();
+        }
+
+        square = map.getSquare();
+
+        protestorsBlockVerticalStreet = new Random().nextBoolean(); // to determine protestors direction
+        randomStreetIndex = new Random().nextInt(MAP_SIZE); // random street
+        int counter = 0;
+
+        while (counter <= THRESHOLD) { // Threshold added so that this does not get stuck in an infinite loop
+            int startIndex = getIndexForFirstRouteSquare();
+
+            if (protestorsCanExist(startIndex)) {
+                return allProtesters;
+            }
+
+            protestorsBlockVerticalStreet = new Random().nextBoolean();
+            randomStreetIndex = new Random().nextInt(MAP_SIZE);
+            counter++;
+        }
+
+        return allProtesters;
+    }
+
+    // Helper Methods
+    private static MapDto getFirstMap(IMapService mapService) {
         MapDto map = null;
         try {
             map = mapService.getAllMap().get(0);
         } catch (JSONException | JsonProcessingException e) {
             e.printStackTrace();
         }
+        return map;
+    }
 
-        if(map == null) return new ArrayList<>(); // Map should not be null
-
-        SquareDto[][] square = map.getSquare();
-        boolean isColumn = new Random().nextBoolean();
-        int rowOrColumnIndex = new Random().nextInt(30);
-
-        int threshold = 15;
-        int counter = 0;
-
-        while (counter <= threshold) { // Threshold added so that this does not get stuck in an infinite loop
-            int startIndex = 0;
-            while (square[isColumn ? startIndex : rowOrColumnIndex][isColumn ? rowOrColumnIndex : startIndex].getValue() != 1 && startIndex < 30) {
-                startIndex++;
-            }
-            for (int varyingIndex = startIndex; varyingIndex < 30; varyingIndex++) {
-                if (square[isColumn ? varyingIndex : rowOrColumnIndex][isColumn ? rowOrColumnIndex : varyingIndex].getValue() == 1) {
-                    int blockLength = getLengthConsecutiveBlocks(varyingIndex, rowOrColumnIndex, isColumn, square);
-
-                    // If there is more than 1 square of route consecutively
-                    if (blockLength > 1) {
-                        ArrayList<AvatarDto> addedAvatars = createProtestors(varyingIndex, rowOrColumnIndex, blockLength, isColumn);
-                        allProtesters.addAll(addedAvatars);
-                        return allProtesters;
-                    }
-                }
-            }
-            counter++;
-            isColumn = new Random().nextBoolean();
-            rowOrColumnIndex = new Random().nextInt(30);
+    private static int getIndexForFirstRouteSquare() {
+        int startIndex = 0;
+        SquareDto nextSquare = getNextSquare(startIndex);
+        while (!nextSquare.isRoute() && startIndex < MAP_SIZE) {
+            startIndex++;
+            nextSquare = getNextSquare(startIndex);
         }
 
-        return allProtesters;
+        return startIndex;
+    }
+
+    private static SquareDto getNextSquare(int startIndex) {
+        int nextY = protestorsBlockVerticalStreet ? startIndex : randomStreetIndex;
+        int nextX = protestorsBlockVerticalStreet ? randomStreetIndex : startIndex;
+        return square[nextY][nextX];
+    }
+
+    private static boolean protestorsCanExist(int startIndex) {
+        for (int varyingIndex = startIndex; varyingIndex < MAP_SIZE; varyingIndex++) {
+            int nextY = protestorsBlockVerticalStreet ? varyingIndex : randomStreetIndex;
+            int nextX = protestorsBlockVerticalStreet ? randomStreetIndex : varyingIndex;
+            if (square[nextY][nextX].isRoute()) {
+                int blockLength = getLengthConsecutiveBlocks(varyingIndex, protestorsBlockVerticalStreet);
+
+                // If there is more than 1 square of route consecutively
+                if (blockLength > 1) {
+                    ArrayList<AvatarDto> addedAvatars = createProtestors(varyingIndex, blockLength);
+                    allProtesters.addAll(addedAvatars);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     // Returns the length of the consecutive block
-    private static int getLengthConsecutiveBlocks(int startIndex, int randomIndex, boolean isColumn, SquareDto[][] square) {
-        for (int i = startIndex + 1; i < 30; i++) {
-            SquareDto adjacentBlock = square[isColumn ? i : randomIndex][isColumn ? randomIndex : i];
+    private static int getLengthConsecutiveBlocks(int startIndex, boolean isColumn) {
+        for (int i = startIndex + 1; i < MAP_SIZE; i++) {
+            int adjacentY = isColumn ? i : randomStreetIndex;
+            int adjacentX = isColumn ? randomStreetIndex : i;
+            SquareDto adjacentBlock = square[adjacentY][adjacentX];
             if (adjacentBlock.getValue() != 1)
                 return i - startIndex;
 
@@ -68,7 +111,7 @@ public class ManifestationManagement {
     }
 
     // Create manifestation from start x for a length of block length (random index indicates the row or column index)
-    private static ArrayList<AvatarDto> createProtestors(int startIndex, int randomIndex, int blockLength, boolean isColumn) {
+    private static ArrayList<AvatarDto> createProtestors(int startIndex, int blockLength) {
         ArrayList<AvatarDto> listOfProtestors = new ArrayList<>();
 
         int minProtestors = 50;
@@ -78,12 +121,12 @@ public class ManifestationManagement {
             // Create protestors for the entire length of the block with routes (street)
             for (int counter = startIndex; counter <= startIndex + blockLength - 1 && totalProtestors < minProtestors; counter++) {
                 AvatarDto protestor = new AvatarDto();
-                protestor.setX(isColumn ? randomIndex : counter);
-                protestor.setY(isColumn ? counter : randomIndex);
-                protestor.setImage("../assets/images/protester.png");
+                protestor.setX(protestorsBlockVerticalStreet ? randomStreetIndex : counter);
+                protestor.setY(protestorsBlockVerticalStreet ? counter : randomStreetIndex);
+                protestor.setImage(PROTESTERS_IMAGE);
                 totalProtestors++;
                 protestor.setName("Strikers" + totalProtestors);
-                protestor.setType(5);
+                protestor.setType(SquareType.PROTESTOR.value);
 
                 listOfProtestors.add(protestor);
             }
--- src/main/java/ca/onepoint/yul/dto/SquareDto.java
+++ src/main/java/ca/onepoint/yul/dto/SquareDto.java
@@ -1,9 +1,14 @@
 package ca.onepoint.yul.dto;
 
+import ca.onepoint.yul.classes.SquareType;
 import lombok.Data;
 
 @Data
 public class SquareDto {
     private Integer value;
     private String image;
+
+    public boolean isRoute(){
+        return   value == SquareType.ROUTE.value;
+    }
 }
