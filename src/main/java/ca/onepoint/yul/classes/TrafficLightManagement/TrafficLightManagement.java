b68edd01f398ffa41bd78ada73b3ea41e6829e2a
Viveka <viveka98@hotmail.ca>
2021-02-01 15:32:01 -0500

    Extracted code repetitive code into methods (using enums to their potential)

--- src/main/java/ca/onepoint/yul/classes/TrafficLightManagement/TrafficLightManagement.java
+++ src/main/java/ca/onepoint/yul/classes/TrafficLightManagement/TrafficLightManagement.java
@@ -11,11 +11,10 @@ import java.util.List;
 // Singleton static class to hold states
 
 public class TrafficLightManagement {
+    public static final String CROSSROADS = "croisement";
     public static TrafficLight[][][] allTrafficLights;
-    public static boolean isRushHour = false;
 
     public static void setUpTrafficLights(IMapService mapService) {
-
         try {
             List<MapDto> allMaps = mapService.getAllMap();
             MapDto montrealMap = allMaps.get(0);
@@ -24,39 +23,35 @@ public class TrafficLightManagement {
             //Create 2D map that represents all the possible positions the map (each position has a traffic light array)
             allTrafficLights = new TrafficLight[allSquares[0].length][allSquares.length][6];
 
-
             for (int y = 0; y < allSquares.length; y++) {
                 for (int x = 0; x < allSquares[y].length; x++) {
-                    // Find crossroads
                     SquareDto square = allSquares[y][x];
 
-                    if (square.getImage().contains("croisement")) {
-                        // Create car traffic lights
-                        TrafficLight horizontalCarTrafficLight = new TrafficLight(TrafficLight.TrafficLightType.CAR, TrafficLight.Direction.HORIZONTAL);
-                        TrafficLight verticalCarTrafficLight = new TrafficLight(TrafficLight.TrafficLightType.CAR, TrafficLight.Direction.VERTICAL);
-                        TrafficLight leftTurnCarTrafficLight = new TrafficLight(TrafficLight.TrafficLightType.CAR, TrafficLight.Direction.LEFT_TURN);
-                        TrafficLight rightTurnCarTrafficLight = new TrafficLight(TrafficLight.TrafficLightType.CAR, TrafficLight.Direction.RIGHT_TURN);
-
-                        //TODO: Add pedestrian traffic lights
-
-
-                        // Configure lights to be opposite
-
-                        // Initialize the timer for all of the traffic lights
-                        horizontalCarTrafficLight.initTimer();
-                        verticalCarTrafficLight.initTimer();
-                        //TODO: Init pedestrian traffic lights
-
-                        // Add new created traffic lights to the list of all traffic lights
-                        allTrafficLights[y][x][TrafficLight.Direction.HORIZONTAL.value] = horizontalCarTrafficLight;
-                        allTrafficLights[y][x][TrafficLight.Direction.VERTICAL.value] = verticalCarTrafficLight;
-                        allTrafficLights[y][x][TrafficLight.Direction.LEFT_TURN.value] = leftTurnCarTrafficLight;
-                        allTrafficLights[y][x][TrafficLight.Direction.RIGHT_TURN.value] = rightTurnCarTrafficLight;
+                    // Find crossroads
+                    if (square.getImage().contains(CROSSROADS)) {
+                        TrafficLight[] allCarTrafficLights = createTrafficLightsFor(TrafficLight.TrafficLightType.CAR);
+                        allTrafficLights[y][x] = allCarTrafficLights;
+                        initTimersAt(x, y);
                     }
                 }
             }
         } catch (JSONException | JsonProcessingException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static TrafficLight[] createTrafficLightsFor(TrafficLight.TrafficLightType type) {
+        TrafficLight.Direction[] allDirections = TrafficLight.Direction.values();
+        TrafficLight[] carTrafficLights = new TrafficLight[allDirections.length];
+        for (TrafficLight.Direction direction : allDirections) {
+            carTrafficLights[direction.value] = new TrafficLight(type, direction);
+        }
+        return carTrafficLights;
+    }
+
+    private static void initTimersAt(int x, int y) {
+        for (TrafficLight trafficLight : allTrafficLights[y][x]) {
+            trafficLight.initTimer();
         }
     }
 
