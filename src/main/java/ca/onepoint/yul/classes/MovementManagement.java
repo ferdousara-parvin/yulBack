7e2d94f45aeb3f2b4c1140b91ef17dd4b0787343
Viveka <viveka98@hotmail.ca>
2021-02-01 17:09:21 -0500

    Introduced constants in the movement management

--- src/main/java/ca/onepoint/yul/classes/MovementManagement.java
+++ src/main/java/ca/onepoint/yul/classes/MovementManagement.java
@@ -9,6 +9,20 @@ import java.util.List;
 
 public class MovementManagement {
 
+    // X-Y coordinates of known locations
+    public static final int START_X = 8;
+    public static final int START_Y = 9;
+    public static final int METRO_ENTRANCE_X = 9;
+    public static final int METRO_ENTRANCE_Y = 9;
+    public static final int METRO_EXIT_X = 15;
+    public static final int METRO_EXIT_Y = 25;
+    public static final int STARBUCKS_X = 3;
+    public static final int STARBUCKS_Y = 21;
+    public static final int BURGER_KING_X = 16;
+    public static final int BURGER_KING_y = 14;
+    public static final int ONEPOINT_X = 13;
+    public static final int ONEPOINT_Y = 18;
+
     public static List<SquareNode> pathToDestination = new ArrayList<>();
     public static int moveCounter;
 
@@ -22,10 +36,10 @@ public class MovementManagement {
     public static void getPathToOnePoint(SquareDto[][] map, AvatarDto myAvatar) {
         pathToDestination = new ArrayList<>();
 
-        List<SquareNode> pathToMetroEntrance = getPathToTarget(map, 8, 9, 9,9);
-        List<SquareNode> pathToStarbucks = getPathToTarget(map,  15,25, 3, 21);
-        List<SquareNode> pathToBurgerKing = getPathToTarget(map,   3, 21, 16, 14);
-        List<SquareNode> pathToOnePoint = getPathToTarget(map,  16,14,13, 18);
+        List<SquareNode> pathToMetroEntrance = getPathToTarget(map, START_X, START_Y, METRO_ENTRANCE_X, METRO_ENTRANCE_Y);
+        List<SquareNode> pathToStarbucks = getPathToTarget(map, METRO_EXIT_X, METRO_EXIT_Y, STARBUCKS_X, STARBUCKS_Y);
+        List<SquareNode> pathToBurgerKing = getPathToTarget(map,   STARBUCKS_X, STARBUCKS_Y, BURGER_KING_X, BURGER_KING_y);
+        List<SquareNode> pathToOnePoint = getPathToTarget(map,  BURGER_KING_X,BURGER_KING_y, ONEPOINT_X, ONEPOINT_Y);
 
         pathToDestination.addAll(pathToMetroEntrance);
         pathToDestination.addAll(pathToStarbucks);
